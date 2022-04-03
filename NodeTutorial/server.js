8ccfabe70a35f38e90d424920f6342e8f3d16415
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-04-03 21:30:08 +0200

    NodeTutorial

---
+++ NodeTutorial/server.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var fs = require('fs');
+
+const server = http.createServer();
+server.on('request', (request, response) => {
+    const { method, url } = request;
+    const { headers } = request;
+    const userAgent = headers['user-agent'];
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        // at this point, `body` has the entire request body stored in it as a string
+    });
+    request.on('error', (err) => {
+        // This prints the error message and stack trace to `stderr`.
+        console.error(err.stack);
+    });
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', (err, data) => {
+		if (err) throw err;
+		response.end(data);
+	});
+    // response.setHeader('Content-Type', 'application/json');
+
+    response.on('error', (err) => {
+        console.error(err);
+    });
+
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    const responseBody = { headers, method, url, body };
+    response.write(JSON.stringify(responseBody));
+    response.end();
+});
+server.listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
