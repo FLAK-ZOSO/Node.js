8f7097d64fd1bfcdde848ededceb16480a783ac5
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-04-04 17:35:55 +0200

    Update server2.js

--- NodeTutorial/server2.js
+++ NodeTutorial/server2.js
@@ -1,20 +1,20 @@
 const http = require('http');
 
 var server = http.createServer((request, response) => {
-  request.on('error', (err) => {
-    console.error(err);
-    response.statusCode = 400;
-    response.end();
-  });
-  response.on('error', (err) => {
-    console.error(err);
-  });
-  if (request.method === 'POST' && request.url === '/echo') {
-    request.pipe(response);
-  } else {
-    response.statusCode = 404;
-    response.end();
-  }
+	request.on('error', (err) => {
+		console.error(err);
+		response.statusCode = 400;
+		response.end();
+	});
+	response.on('error', (err) => {
+		console.error(err);
+	});
+	if (request.method === 'POST' && request.url === '/') {
+		request.pipe(response);
+	} else {
+		response.statusCode = 404;
+		response.end();
+	}
 })
 server.listen(8080, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8080/');
