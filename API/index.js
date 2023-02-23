bc6c7443d783f70212d3e97109f20f9c089c7b07
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2023-02-23 18:30:22 +0100

    Most HTTP methods implemented... almost correct í¸³

--- API/index.js
+++ API/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-wasps = [
+wasps = new Array(
     '🐛', '🐝', '🐜',
     '🦂', '🦗', '🦟',
     '🐞', '🦋', '🐌'
-];
+);
 
 app.use(express.json());
 app.listen(PORT, () => {
@@ -23,7 +23,51 @@ app.get('/wasps', (_, res) => {
         size: wasps.length
     });
 });
-app.post('/wasps/:index', (req, res) => {
+app.delete('/wasps', (_, res) => {
+    temp = wasps;
+    wasps = [];
+
+    res.status(200).send({
+        insects: temp,
+        size: temp.length
+    });
+});
+app.put('/wasps', (req, res) => {
+    const { insects } = req.body;
+
+    if (!insects) {
+        // 422: Unprocessable Entity
+        res.status(422).send({
+            message: 'You must provide an array of insects'
+        });
+    } else {
+        temp = wasps;
+        wasps = insects;
+        res.status(200).send({
+            old: temp,
+            insects: wasps,
+            size: wasps.length
+        });
+    }
+});
+app.post('/wasps', (req, res) => {
+    const { insect } = req.body;
+
+    if (!insect) {
+        // 422: Unprocessable Entity
+        res.status(422).send({
+            message: 'You must provide an insect'
+        });
+    } else {
+        wasps.push(insect);
+        res.status(200).send({
+            added: `${insect} at index ${wasps.length - 1}`,
+            insects: wasps,
+            size: wasps.length
+        });
+    }
+});
+app.patch('/wasps/:index', (req, res) => {
     const { index } = req.params;
     const { insect } = req.body;
 
@@ -38,9 +82,44 @@ app.post('/wasps/:index', (req, res) => {
             message: `Index ${index} is out of bounds`
         });
     } else {
+        temp = wasps[index];
         wasps[index] = insect;
         res.status(200).send({
+            old: temp,
             added: `${insect} at index ${index}`,
+            insects: wasps,
+            size: wasps.length
+        });
+    }
+});
+app.get('/wasps/:index', (req, res) => {
+    const { index } = req.params;
+
+    if (index >= wasps.length) {
+        // 404: Not Found
+        res.status(404).send({
+            message: `Index ${index} is out of bounds`
+        });
+    } else {
+        res.status(200).send({
+            insect: wasps[index]
+        });
+    }
+});
+app.delete('/wasps/:index', (req, res) => {
+    const { index } = req.params;
+    wasp_to_delete = wasps[index];
+
+    if (index >= wasps.length) {
+        // 404: Not Found
+        res.status(404).send({
+            message: `Index ${index} is out of bounds`
+        });
+    } else {
+        wasps.splice(index, 1);
+        res.status(200).send({
+            deleted: `Insect at index ${index}: ${wasp_to_delete}`,
+            insect: wasp_to_delete,
             insects: wasps,
             size: wasps.length
         });
